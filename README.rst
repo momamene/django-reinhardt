7b7b4cd46ef071c5899724e551ab9c2e516f6e3a
momamene <momamene@gmail.com>
2016-07-19 00:57:19 +0000

    First Commit

---
+++ README.rst
@@ -0,0 +1,39 @@
+===============================
+django-reinhardt
+===============================
+
+
+.. image:: https://img.shields.io/pypi/v/django-reinhardt.svg
+        :target: https://pypi.python.org/pypi/django-reinhardt
+
+.. image:: https://img.shields.io/travis/momamene/django-reinhardt.svg
+        :target: https://travis-ci.org/momamene/django-reinhardt
+
+.. image:: https://readthedocs.org/projects/django-reinhardt/badge/?version=latest
+        :target: https://django-reinhardt.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+.. image:: https://pyup.io/repos/github/momamene/django-reinhardt/shield.svg
+     :target: https://pyup.io/repos/github/momamene/django-reinhardt/
+     :alt: Updates
+
+
+Manage object permissions by implementing methods in Django Model
+
+
+* Free software: MIT license
+* Documentation: https://reinhardt.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+---------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
---
+++ reinhardt/tests/settings.py
@@ -0,0 +1,87 @@
+import environ
+
+env = environ.Env()
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '^uykg#pbpl*lv6(+5g-760)&b$3*=e@&w-r$^7f-i1k(!v7w4-'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'reinhardt.tests'
+]
+
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'tests.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+DATABASES = {'default': env.db(default="sqlite:///")}
+
+
+""" AUTHENTICATION """
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'reinhardt.backends.PermissionBackend',
+)
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.9/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATIC_URL = '/static/'
