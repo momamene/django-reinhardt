ae4d782eeaa55f9ad3123f7278bf25b51653b256
momamene <momamene@gmail.com>
2016-08-02 00:35:59 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,11 +38,12 @@ setup(
     zip_safe=False,
     keywords = 'django object logical permission auth authentication reinhardt',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
